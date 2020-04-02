3815b93cb40979ac9ae5eef6ae6da8620576cb5c
Francesc Ferre <francesc@MacBook-Air-de-Francesc.local>
2020-04-02 20:34:09 +0200

    sync form code

--- application.py
+++ application.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index()
-    return "Hello world"
+    return render_template ("index.html")
+
+@app.route("/hello", methods=["POST"])
+def hello()
+    name = request.form.get("name")
+    return render_template ("hello.html", name=name)
